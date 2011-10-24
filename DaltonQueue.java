f2e305c99508729fee44152351fb70a019ab5f3e
annelise <asteel@asteele.com>
2011-10-24 14:19:20 -0400

    f

--- DaltonQueue.java
+++ DaltonQueue.java
@@ -5,12 +5,12 @@ Isabella Giovannini
 Jason Shein
 Katherine Odom
 Malina Buturovic
-Derek Whang
+Derek Whang -_- hohoho
 Annelise Steele <3
 Sophia Edelstein 
  */
 
 public class DaltonQueue extends java.util.Queue
 {
-
+    public class peek
 }
