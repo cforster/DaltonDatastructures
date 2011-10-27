c4cd5bd4b8e0866520ba075fbc868e3213c072fa
Alexandra Mayer <alexandra_mayer@dalton.org>
2011-10-27 12:36:41 -0400

    Not finished reverse!

--- DaltonStack.java
+++ DaltonStack.java
@@ -5,7 +5,7 @@
  Xander Chase
  Charles Forster
  Emily Lovett
-Jeffrey Stern
+ Jeffrey Stern
  Taylor Wright
  Harry DiPirinzio
  Josh Kaye
@@ -25,9 +25,40 @@ public class DaltonStack extends java.util.Stack
       Output: nothing
       Notes: This is an example from charlie
     */
+
+
     public void deleteTop()
     {
 	this.pop();
+    }
+
+    int frequency(int number)
+    {
+	int[] jesus = new int[this.size()];
+	int buddha = 0;
+
+	while(this.empty() = false)
+	    {
+		for (int i = 0; i <= this.size(); int++)
+		    {
+			jesus[i] = this.pop();
+		    }
+	   
+	    }
+	
+	for (int x = 0; x<=jesus.length(); x++)
+	    {
+		if(jesus[x]=number) 
+		    {
+			buddha++;
+		    }
+	   
+	    }
+
+	//push everything back into the stack starting from the top 
+	
+
+
     }
 
 
