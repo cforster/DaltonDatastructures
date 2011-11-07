7b215f55a8ebf1d094e4b61be35c844c6c2bb0bd
c13ko <student@w89233ak9gv.dalton.org>
2011-11-07 14:26:04 -0500

    get commit

--- H-Period/LLTest.java
+++ H-Period/LLTest.java
@@ -5,8 +5,13 @@ public class LLTest
 	LinkedListH myList = new LinkedListH();
 	myList.addfirst(3);
 	myList.addfirst(4);
+	myList.addfirst(5);
 	myList.print(myList.first);
 	System.out.println(myList.size(myList.first));
+
+
+	myList.get(1, myList.first);
+
     }
 
 }
