d08ca373e3ac6b06e35ed3f0d82f3a969d39528c
Maya Klabin <maya_klabin@dalton.org>
2011-11-11 11:56:01 -0500

    fixed errors with charlie, compiles now.  not all working yet

--- I-Period/LinkedListI.java
+++ I-Period/LinkedListI.java
@@ -84,7 +84,7 @@ public class LinkedListI
 	else if (n.pointer.pointer == null) n.pointer = null;    
     }//removelast
 
-<<<<<<< HEAD
+
     //By Michael Zhao
     public boolean retain(int keep, Node n)
     {// Retains only the elements in this list that are contained in the specified collection (optional operation). 
@@ -104,9 +104,11 @@ public class LinkedListI
 		    }
 
 	    }	
+	return false;
+    }
     //By Michael Zhao 
-}//class
-=======
+
+
 
     /**
        Authors: Emily Lovett and Alex Mayer
@@ -168,10 +170,10 @@ public class LinkedListI
 	    }
 	
 
-
+	return null;
 
 
     }//randomize
 
 }//class
->>>>>>> origin/master
+
