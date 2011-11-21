2b7585ed9f91e32a42a6a1ceff042581db82aae3
Sophia Edelstein <c13se@dalton.org>
2011-11-21 14:15:54 -0500

    test class

--- DaltonQueueTest.java
+++ DaltonQueueTest.java
@@ -1,1 +1,16 @@
 // queue test class
+public class DaltonQueueTest extends DaltonQueue
+{ 
+    public static void main(String[] args)
+    { Queue classQueue = new Queue();
+	classQueue.offer(3);
+	classQueue.offer(4);
+	classQueue.length;
+	classQueue.search(3);
+	classQueue.dump;
+	
+
+
+    }
+    
+}
